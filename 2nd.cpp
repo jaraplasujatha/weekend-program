cba33874ef6bc8af4395de6613b2a21d18f84b50
jaraplasujatha <143472658+jaraplasujatha@users.noreply.github.com>
2023-10-31 17:02:46 +0530

    Add files via upload

---
+++ 2nd.cpp
@@ -0,0 +1,39 @@
+# include<stdio.h>
+ int main()
+
+  {
+       for(  inti=0; i<n-1; i++)
+  { 
+      for( j=0; j<n-i-1; j++)
+  {
+     if (arr[j] >arr[j+i])
+     {
+	 // swap arr[j] and arr[j+i]
+	          arr[ j]= arr[j+i];
+	          arr[j+i]
+	      }
+   }
+}
+           int main()
+           {
+           int n;
+           printf("enter the number of elements:");
+           scanf("%d,n");
+           int arr[n];
+           printf("enter %d elements:\n",n)
+           for( i=0; i<n; i++)
+           {
+		   scanf("%d",$arr[]i);
+		   printf("sorted array in ascending order:\n")
+		   {
+		   for (i=0; i<n; i++)
+		   {
+		   printf("%d",arr[1]);
+		   {
+		   	printf("\n");
+		   }
+		     return 0;
+	}
+		   
+           
+           
