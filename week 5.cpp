b95ee4947ce929525b98e6e4557a981d29346e7d
jaraplasujatha <143472658+jaraplasujatha@users.noreply.github.com>
2023-10-31 17:01:50 +0530

    Add files via upload

---
+++ week 5.cpp
@@ -0,0 +1,63 @@
+ # include<stdio.h>
+  int main() 
+  {
+   int n,i;
+    printf( "enter the number of minimum":);
+     scanf("%d", $n)
+      int arr [n];
+      printf("enter the maximum : \n,");
+      for(i=0; i<n; i++)
+	  {
+	  scanf("%d",$arr[1]);
+}
+      int mim=arr[0];
+      int mxm=arr[0];
+      int sum=arr[0];
+       for(i=1; i<n; i++)
+   {
+     if(arr[i] <mim)
+     {
+     	min= arr[i];
+     	{
+		 if(arr[i] >max)
+		 {
+		 max= arr[i];
+		 {
+	   min= arr[i];
+}
+      sum+=arr[i];
+  }
+      float average=( float) sum/n;
+         printf( "minimum: %d\n", min);
+          printf( "maximum:%d\n", max);
+          printf(" sum:%d.2f\n", average );
+      }
+          return 0;
+      }
+	 
+	 
+	 
+    
+    
+	  
+      
+      
+      
+	  
+    
+     
+   
+      
+      
+    
+	  
+	  
+	  
+	
+	
+	  
+	  
+	
+      
+	 
+
